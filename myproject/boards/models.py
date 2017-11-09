7218951dac011547a93f320118708a594b28bc96
Mosaab Ehab <musab.ehab@gmail.com>
2017-11-09 13:52:33 +0200

    Add templates and some tests

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -9,7 +9,7 @@ class Board(models.Model):
     description = models.CharField(max_length=100)
 
     def __str__(self):
-        self.name
+        return self.name
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Board
+
 def home(request):
-    return HttpResponse('hello, world')
+    boards = Board.objects.all()
+    context = {
+        'boards':boards,
+    }
+    return render(request,'home.html',context)
