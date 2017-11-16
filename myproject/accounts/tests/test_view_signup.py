ea34ba1f39c24e59f9165b873ee28bb8b17286c0
Mosaab Ehab <musab.ehab@gmail.com>
2017-11-16 15:19:38 +0200

    modify signup form

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -7,9 +7,12 @@ from django.core.urlresolvers import reverse
 
 from django.urls import resolve
 
-from .views import signup
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+from ..views import signup
+from ..forms import SignUpForm
 # Create your tests here.
 
 class SignUpTest(TestCase):
@@ -29,14 +32,26 @@ class SignUpTest(TestCase):
 
     def test_contins_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
 
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username','email','password1','password2']
+        actual = list(form.fields)
+        self.assertSequenceEqual(actual,expected)
+    
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
         data = {
             'username': 'mosaab',
+            'email': 'mm@dmd.com',
             'password1': 'abcdef12345',
             'password2': 'abcdef12345',
         }
