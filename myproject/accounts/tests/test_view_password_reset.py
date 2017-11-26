bfca6b9388c6d016df6ad442adb33fc66c414d30
mosaab <musab.ehab@gmail.com>
2017-11-26 20:28:35 +0200

    add Password Reset Confirm Tests

--- myproject/accounts/tests/test_view_password_reset.py
+++ myproject/accounts/tests/test_view_password_reset.py
@@ -8,6 +8,11 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
 
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.forms import SetPasswordForm
+
 class PasswordResetTest(TestCase):
     def setUp(self):
         url = reverse('password_reset')
@@ -69,6 +74,36 @@ class PasswordResetDoneTests(TestCase):
     def test_view_function(self):
         view = resolve('/reset/done/')
         self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
+
+
+class PasswordResetConfirmTests(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='john',email='john@doe.com', password='123abcdef')
+        self.uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        self.token = default_token_generator.make_token(user)
+
+        url = reverse('password_reset_confirm', kwargs={'uidb64':self.uid,'token':self.token})
+        
+
+        self.response = self.client.get(url, follow=True)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/{uidb64}/{token}/'.format(uidb64=self.uid, token=self.token))
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetConfirmView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SetPasswordForm)
+
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 3)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 
