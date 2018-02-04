8d83a93999a3f622f9fc8e46756ca4a07a9817ba
mosaab <musab.ehab@gmail.com>
2018-02-04 20:27:17 +0200

    Add some tests

--- myproject/boards/tests/test_view_new_topic.py
+++ myproject/boards/tests/test_view_new_topic.py
@@ -54,9 +54,10 @@ class NewTopicTests(TestCase):
 
 
     def test_new_topic_view_success_status_code(self):
-        url = reverse('new_topic' ,kwargs= {'pk':1 })
+        url = reverse('new_topic', kwargs= {'pk':1 })
         response = self.client.get(url)
-        self.assertEquals(response.status_code , 200)
+        self.assertEquals(response.status_code, 200)
+
 
     def test_new_topic_view_not_found_status_code(self):
         url = reverse('new_topic', kwargs={'pk':99})
@@ -88,3 +89,13 @@ class NewTopicTests(TestCase):
 
         self.assertEquals(response.status_code, 200)
         self.assertTrue(form.errors)
+
+class LoginRequiredNewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description="Django board")
+        self.url = reverse('new_topic', kwargs={'pk':1})
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url = self.url))
