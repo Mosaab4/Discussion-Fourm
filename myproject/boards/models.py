9e3e9b91911628874e37ebe80258293e6d20326a
mosaab <musab.ehab@gmail.com>
2018-02-07 12:35:39 +0200

    add markdown

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
+from django.utils.html import mark_safe
+from markdown import markdown
 
 class Board(models.Model):
     name = models.CharField(max_length=30,unique=True)
@@ -40,7 +42,8 @@ class Post(models.Model):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
 
-
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
 
 
 
