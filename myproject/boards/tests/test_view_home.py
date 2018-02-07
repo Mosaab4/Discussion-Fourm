6abe2c8c1d2ebdf8d07ed553170bd0cd3c94b4ba
mosaab <musab.ehab@gmail.com>
2018-02-07 08:18:32 +0200

    add some CBV

--- myproject/boards/tests/test_view_home.py
+++ myproject/boards/tests/test_view_home.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.contrib.auth.models import User
 
-from ..views import home,board_topics, new_topic
+from ..views import home,board_topics, new_topic, BoardListView
 from ..models import Board, Topic,Post
 
 class HomeTests(TestCase):
@@ -20,8 +20,9 @@ class HomeTests(TestCase):
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEquals(view.func, home)
+        self.assertEquals(view.func.view_class, BoardListView)
 
+        
     def test_home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse('board_topics', kwargs={"pk": self.board.pk })
         self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
